96ecda81538120ab1a80aa8ce1bdf8906f4d2ba9
Carlos Rodriguez <139814946+CarlosRodriguez14@users.noreply.github.com>
2023-12-04 19:14:17 +0000

    Last Commit

--- test.js
+++ test.js
@@ -22,7 +22,7 @@ test("One euro should be 1.07 dollars", function() {
     const expected = 3.5 * 1.07;
 
     // Hago mi comparación (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+    expect(dollars).toBe(expected); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
 })
 
 test("1.07 dollars should be 156.5 yen", function() {
@@ -36,7 +36,7 @@ test("1.07 dollars should be 156.5 yen", function() {
     const expected = 5 * 156.5;
 
     // Hago mi comparación (la prueba)
-    expect(fromDollarToYen(5)).toBe(782.5); // 5 dolares son 156.5 yen, entonces 5 dolares deberían ser = (5 * 156.5)
+    expect(yens).toBe(expected); // 1 dolar son 156.5 yen, entonces 5 dolares deberían ser = (5 * 156.5)
 })
 
 test("156.5 Yens should be 0.87 pounds", function() {
@@ -50,5 +50,5 @@ test("156.5 Yens should be 0.87 pounds", function() {
     const expected = 313 * 0.87;
 
     // Hago mi comparación (la prueba)
-    expect(fromYentoPound(313)).toBe(272.31); // 156.5 yens son 0.87 pounds, entonces 313 yens deberían ser = (313 * 0.87)
+    expect(pounds).toBe(expected); // 156.5 yens son 0.87 pounds, entonces 313 yens deberían ser = (313 * 0.87)
 })
